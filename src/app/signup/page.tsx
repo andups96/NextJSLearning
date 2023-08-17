f19a18a5feba8133d342e2bbd538ff792f5fdfde
andups96 <anupdeshmukh96@gmail.com>
2023-08-17 20:45:44 +0530

    added profile,login,signup pages & dbConfig.ts

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,59 @@
+"use client"
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { Axios } from 'axios';
+
+export default function SignUp() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: ""
+  });
+
+  const onSignup = async () => {
+
+  }
+
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+      <h1>
+        signup
+      </h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder='username'
+      />
+
+      <label htmlFor="email">email</label>
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder='email'
+      />
+
+      <label htmlFor="password">password</label>
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="password"
+        type="text"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder='password'
+      />
+
+      <button 
+      onClick={onSignup}
+      className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
+        SignUp Here</button>
+
+    <Link href="/login">Visit Login Page</Link>
+    </div >
+  )
+}
