3e611150f66fe6afa1065414548a884fa1c3e682
andups96 <anupdeshmukh96@gmail.com>
2023-08-19 17:53:22 +0530

    working logout without middleware

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,21 @@
-import React from 'react'
+"use client"
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import toast from 'react-hot-toast';
 
 export default function ProfilePage() {
+    const router = useRouter();
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout");
+            toast.success("Logout succesful");
+            router.push('/login');
+        } catch (error: any) {
+            console.log(error.message);
+            toast.error(error.message);
+        }
+    }
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>
@@ -8,6 +23,12 @@ export default function ProfilePage() {
             </h1>
             <hr />
             <p>Profile Page.</p>
+            <hr />
+            <button
+                onClick={logout}
+                className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Logout
+            </button>
         </div>
     )
 }
