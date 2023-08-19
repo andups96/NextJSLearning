40cc241e0f514987f9e576c7ac981fd7611b8a15
andups96 <anupdeshmukh96@gmail.com>
2023-08-19 11:17:56 +0530

    signup and login backend completed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,28 +1,51 @@
 "use client"
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { Axios } from 'axios';
+import axios from 'axios';
+import { toast } from 'react-hot-toast/headless';
 
 export default function Login() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: ""
   });
 
-  const onLogin = async () => {
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login Success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   }
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <h1>
-        Login
+        {loading ? "Processing" : "Login"}
       </h1>
       <hr />
 
       <label htmlFor="email">email</label>
-      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="email"
         type="text"
         value={user.email}
@@ -31,7 +54,7 @@ export default function Login() {
       />
 
       <label htmlFor="password">password</label>
-      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="password"
         type="text"
         value={user.password}
@@ -39,12 +62,12 @@ export default function Login() {
         placeholder='password'
       />
 
-      <button 
-      onClick={onLogin}
-      className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
-        SignUp Here</button>
+      <button
+        onClick={onLogin}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
+        Login</button>
 
-    <Link href="/signup">Visit Signup Page</Link>
+      <Link href="/signup">Visit Signup Page</Link>
     </div >
   )
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,28 +1,51 @@
 "use client"
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { Axios } from 'axios';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 export default function SignUp() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
     username: ""
   });
-
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const onSignup = async () => {
-
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success ", response.data);
+      router.push("/login");
+      
+    } catch (error: any) {
+      toast.error(error.message);
+      console.log("Signup failed ", error.message);
+    } finally {
+      setLoading(false);
+    }
   }
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+      setButtonDisabled(false);
+    }
+    else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <h1>
-        signup
+        {loading ? "Processing" : "SignUp"}
       </h1>
       <hr />
       <label htmlFor="username">username</label>
-      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="username"
         type="text"
         value={user.username}
@@ -31,7 +54,7 @@ export default function SignUp() {
       />
 
       <label htmlFor="email">email</label>
-      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="email"
         type="text"
         value={user.email}
@@ -40,7 +63,7 @@ export default function SignUp() {
       />
 
       <label htmlFor="password">password</label>
-      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      <input className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
         id="password"
         type="text"
         value={user.password}
@@ -48,12 +71,13 @@ export default function SignUp() {
         placeholder='password'
       />
 
-      <button 
-      onClick={onSignup}
-      className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
-        SignUp Here</button>
+      <button
+        onClick={onSignup}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
+        {buttonDisabled ? "SignUp Disabled" : "SignUp Here"}
+      </button>
 
-    <Link href="/login">Visit Login Page</Link>
+      <Link href="/login">Visit Login Page</Link>
     </div >
   )
 }
